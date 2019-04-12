0524c26bdae6a0841e7400badfc9b5776f3d83ad
miguel <miguel.trujillo.navarro.alu@iesjulioverne.es>
2019-04-12 12:49:04 +0200

    rrtetfh

--- After the Virus/src/Modelo/MazoZombies.java
+++ After the Virus/src/Modelo/MazoZombies.java
@@ -6,7 +6,13 @@
 package Modelo;
 
 import Modelo.Cartas.Carta;
+import Modelo.Cartas.Zombie;
+import Modelo.Cartas.Zombie1;
+import Modelo.Cartas.Zombie2;
+import Modelo.Cartas.Zombie3;
+import Modelo.Cartas.Zombie4;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
@@ -14,17 +20,62 @@ import java.util.ArrayList;
  */
 public class MazoZombies extends Mazo {
 
-    ArrayList<Carta> MazoZombie = new ArrayList();
-
+    ArrayList<Carta> MazoZombie;
+    ArrayList<Carta> descarteZombies;
+    Zombie zombie;
     //Se encarga de administrar los monstruos, debe poner Zombies en su array de escenario y en cada turno restaurar el mazo en el orden correcto
+    
     public MazoZombies(Escenario escenario) {
         super(escenario);
+        MazoZombie = new ArrayList<>();
+        descarteZombies = new ArrayList<>();
+        crearMazo();
     }
-
+    public void crearMazo(){
+        for(int i=1;i<=4;i++){
+            switch(i){
+                case 1:
+                    for(int z1=0;z1<5;z1++){
+                        Zombie zombie1 = new Zombie1(0, "Z1", escenario, 0, 0);
+                        MazoZombie.add(zombie1);
+                    }
+                    break;
+                case 2:
+                    for(int z1=0;z1<4;z1++){
+                        Zombie zombie2 = new Zombie2(0, "Z2", escenario, 0, 0);
+                        MazoZombie.add(zombie);
+                    }
+                    break;
+                case 3:
+                    for(int z1=0;z1<3;z1++){
+                        Zombie zombie3 = new Zombie3(0, "Z3", escenario, 0, 0);
+                        MazoZombie.add(zombie);
+                    }
+                    break;
+                case 4:
+                    for(int z1=0;z1<2;z1++){
+                        Zombie zombie4 = new Zombie4(0, "Z4", escenario, 0, 0);
+                        MazoZombie.add(zombie);
+                    }
+                    break;
+            }
+        }
+    }
+    /*Metodo que se encargará de sacar 1,2 o 3 cartas del mazo de zombies*/
+    public void cogerRandom(){
+        
+        int coge=(int)Math.random()*3+1;
+        for(int i=0;i<coge;i++){
+            escenario.mazoJugador.getMazoJugador().add(MazoZombie.get(0));
+        }
+    }
     public void Restaurar() {
+        
+        
     }
 
-    public void Introducir() {
+    public void introducir(){
+        
     }
 
 }
