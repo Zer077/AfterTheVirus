ceafbc3c8513ca97c02e96e439f8727d7126e415
miguel <miguel.trujillo.navarro.alu@iesjulioverne.es>
2019-04-12 12:17:52 +0200

    1

--- After the Virus/src/Controlador/CompraCartas.java
+++ After the Virus/src/Controlador/CompraCartas.java
@@ -7,6 +7,7 @@ package Controlador;
 
 import Modelo.Cartas.Carta;
 import Modelo.Escenario;
+import java.util.Scanner;
 
 /**
  *
@@ -32,7 +33,14 @@ public class CompraCartas {
     //En este metodo le paso un array de cartas que es el costo de comprar X carta, esa X carta a comprar es el propio método el que contea cuanto vale y cual quiere comprar, si la cantidad de cartas pasadas es mayor las devuelve a la mano
     //En este caso es este método el encargado de eliminar las cartas de la mano
     public boolean compra(Carta[] carta) {
-        
+                Scanner sc=new Scanner(System.in);
+        System.out.println("Elige la carta a comprar");
+        escenario.vista.verAreaExploracion();
+        int numero=sc.nextInt();
+        if (carta.length==escenario.arrayEscenario.get(numero).getPrecio()){
+            return true;
+        }
+        return false;
         
         
         return false;
