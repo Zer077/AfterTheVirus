e0d520f2d5925b9d1076c69810a51fc9b0ac429f
Jose <atochejoseangel@gmail.com>
2019-04-22 12:02:44 +0200

    nueva version

--- After the Virus/src/Controlador/Controlador.java
+++ After the Virus/src/Controlador/Controlador.java
@@ -6,6 +6,7 @@
 package Controlador;
 
 import Modelo.*;
+import Modelo.Personaje.Personaje;
 
 /**
  *
@@ -14,21 +15,22 @@ import Modelo.*;
 public class Controlador extends Escenario {
 
     int contador;
+
     //Debe encargarse de que funcione la aplicacion con las diversas mecánicas
     //NOTA IMPORTANTE, AL PRINCIPIO DEL JUEGO DEBEMOS DARLE ALEATORIAMENTE ENTRE 1 Y 3 CARTAS ZOMBIE AL MAZO JUGADOR
     //CREAR 3 CONTROLADORES  PARA LOS 3 MODOS DE JUEGO
-    public Controlador() {
-
+    public Controlador(Personaje p) {
+        personaje = p;
     }
 
     public void controlador1a() {
         //preparacion: comienza con 1 oleada (añadir 1-3 cartas zombi random)
-        
-        int coge = (int) Math.random() * 3 + 1;
-        mazoJugador.getMazoJugador().add(mazoZombies.introducirAleatorio(coge));
 
-        do{
-        /*1- Se establece las cartas de Jugador
+        int coge = (int) Math.random() * 3 + 1;
+        //mazoJugador.getMazoJugador().add(mazoZombies.introducirAleatorio(coge));
+
+        do {
+            /*1- Se establece las cartas de Jugador
         2- Se establecen los demás mazos
         3-Se baraja las cartas del jugador
         4- La mano del jugador coje 5 cartas y la puede usar para varias cosas:
@@ -39,24 +41,24 @@ public class Controlador extends Escenario {
         Al acabar las ronda comienzan los zombies, ronda 1 es 1 carta Zombie, ronda 2, 2 cartas Zombie…
         5-Se sacan cartas Zombie y meten en el mazo de jugador
         6-Se baraja las cartas del jugador
-        */    
-           
-            
-        for (int i = 0; i < areaJugable.getArrayJugables().size(); i++) {
-                if (areaJugable.getArrayJugables().get(i).isActiva()==true)
+             */
+
+            for (int i = 0; i < areaJugable.getArrayJugables().size(); i++) {
+                if (areaJugable.getArrayJugables().get(i).isActiva() == true) {
                     contador++;
+                }
             }
-        
-        if (personaje.isCabeza()==true){
-            System.out.println("HAS MUERTO");
-        }else if(personaje.isCabeza()==false && contador >=6){
-            System.out.println("HAS GANADO");          
-        }
-        
-       Ronda++;
-       contador=0;
-       
-       }while(personaje.isCabeza()==false);   
+
+            if (personaje.isCabeza() == true) {
+                System.out.println("HAS MUERTO");
+            } else if (personaje.isCabeza() == false && contador >= 6) {
+                System.out.println("HAS GANADO");
+            }
+
+            Ronda++;
+            contador = 0;
+
+        } while (personaje.isCabeza() == false);
     }
 
     public void controlador1b() {
@@ -68,5 +70,5 @@ public class Controlador extends Escenario {
         //objetivo: acaba el turno con 6 supervivientes rescatados y una instalacion preparada
         //preparacion: comienza con 1 oleada.
     }
-    
+
 }
