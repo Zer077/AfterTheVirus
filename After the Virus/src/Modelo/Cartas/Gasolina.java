1bffae1acb82b3b2a886f753d3df76d6c965f965
usuario <usuario@usuario-ALDA>
2019-04-22 11:11:57 +0200

    gasolina terminada

--- After the Virus/src/Modelo/Cartas/Gasolina.java
+++ After the Virus/src/Modelo/Cartas/Gasolina.java
@@ -14,30 +14,20 @@ import Modelo.Escenario;
 public class Gasolina extends Carta {
 
     public Gasolina(int precio, String nombre, Escenario Escenario, int tipo, int activacion) {
-        super(2, "Gasolina",Escenario, 4, 1);
+        super(2, "Gasolina", Escenario, 4, 1);
     }
 
     @Override
     public void action() {
-        
+
         for (int i = 0; i < escenario.areaJugable.getArrayJugables().size(); i++) {
-            
-            if (escenario.areaJugable.getArrayJugables().get(i).getTipo() == 10) {
-                escenario.areaJugable.getArrayJugables().get(i).setActiva(true);
 
-            }
-            
-            if (escenario.areaJugable.getArrayJugables().get(i).getTipo() == 14) {
-                escenario.areaJugable.getArrayJugables().get(i).setActiva(true);
+            escenario.areaJugable.BuscarCartaTipo(10).setActiva(true);
+            escenario.areaJugable.BuscarCartaTipo(14).setActiva(true);
+            escenario.areaJugable.BuscarCartaTipo(1).setActiva(true);
+            escenario.areaJugable.BuscarCartaTipo(2).setActiva(true);
+            escenario.areaJugable.BuscarCartaTipo(3).setActiva(true);
 
-            }
-            
-            
-            if (escenario.areaJugable.getArrayJugables().get(i).getTipo() == 1 || escenario.areaJugable.getArrayJugables().get(i).getTipo() ==  2 || escenario.areaJugable.getArrayJugables().get(i).getTipo() == 3) {
-                escenario.areaJugable.getArrayJugables().get(i).setActiva(true);
-
-            }
-            
             escenario.areaJugable.EliminarCarta(this);
         }
     }
