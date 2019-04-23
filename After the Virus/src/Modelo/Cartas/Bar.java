6f692c58277be3e4bf0d54e542ae8134c410c336
usuario <usuario@usuario-ALDA>
2019-04-23 11:50:48 +0200

    bar y tunel terminado

--- After the Virus/src/Modelo/Cartas/Bar.java
+++ After the Virus/src/Modelo/Cartas/Bar.java
@@ -24,13 +24,18 @@ public class Bar extends Carta {
         if ((escenario.areaJugable.BuscarCartaNombre("Refugio").getNombre() == "Refugio") && (escenario.areaJugable.BuscarCartaNombre("Refugio").isActiva()) && (escenario.PersonasSalvadas > 0)) {
             System.out.println("Elige la zona que quieres curarte");
 
-            if (escenario.personaje.isBrazo() == true) {
-                System.out.println("El brazo esta dañado");
+            if ((escenario.personaje.isBrazo() == true) && (escenario.personaje.isPierna() == true) ) {
+                System.out.println("El brazo y la pierna estan dañadas");
             }
 
-            if (escenario.personaje.isPierna() == true) {
-                System.out.println("La pierna esta dañada");
+            else if (escenario.personaje.isBrazo() == true) {
+                System.out.println("El brazo esta dañado ");
+                
             }
+            else if  (escenario.personaje.isPierna() == true){
+            System.out.println("La pierna esta dañada");
+            }
+            
 
             System.out.println("1. Brazo 2.Pierna");
             Scanner sc = new Scanner(System.in);
