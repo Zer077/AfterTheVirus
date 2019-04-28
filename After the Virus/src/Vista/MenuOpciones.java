17e4c508af34c248a0a7a5bc1941a3d25a525d7b
jmara <jmara@192.168.56.1>
2019-04-28 21:49:29 +0200

    Menu Update

--- After the Virus/src/Vista/MenuOpciones.java
+++ After the Virus/src/Vista/MenuOpciones.java
@@ -5,7 +5,10 @@
  */
 package Vista;
 
+import Modelo.Escenario;
 import Modelo.Mano;
+import Vista.*;
+import java.util.Scanner;
 
 /**
  *
@@ -15,30 +18,46 @@ public class MenuOpciones {
 
 //me permite elegir las cartas que quiero usar y la opcion para estas
     Mano mano;
+    Escenario escenario;
 
     public MenuOpciones(Mano mano) {
         this.mano = mano;
     }
 
     public void Menu() {
-        //1-Usar de las cartas inactivas (activar y usar),
-
-        System.out.println("activar cartas");
-        //2-Usar para colocar (inactiva) (Si se coloca una carta de costo 0 de activacion activar automáticamente)
-        System.out.println("Colocar cartas");
-        //3-Usar para comprar
-        System.out.println("Usar cartas seleccionadas para comprar");
-        //4-Usar para Explorar
-        System.out.println("Usar cartas para explorar");
+        System.out.println("MENU OPCIONES");
+        System.out.println("1- Usar cart coste 0");
+        System.out.println("2- Colocar carta");
+        System.out.println("3- Usar cartas seleccionadas para activar");
+        System.out.println("4- Usar cartas para explorar");
+        System.out.println("5- Usar cartas para comprar");
+        System.out.println("6- Salir");
+        System.out.println("Elige una opción: ");
+        ElegirOpcion();
     }
 
     //Elige una de las diversas opciones del juego
     public int ElegirOpcion() {
-        return 0;
+        Scanner sc = new Scanner(System.in);
+        int opcionElegida = sc.nextInt();
+        return opcionElegida; 
     }
 
     //muestra las cartas y permite Elegir todas las cartas que deseas y devuelves el numero de estas 
     public int[] ElegirCartas() {
+        Scanner sc = new Scanner(System.in);
+        escenario.vista.verMano();
+        System.out.println("Selecciona el numero de cartas que quieras usar: ");
+        int numeroCartas = sc.nextInt();
+        if (mano.numeroCartas() < numeroCartas){
+            System.out.println("El numero de cartas en tu mano es menor que el numero de cartas que quieres seleccionar.");
+            ElegirCartas();
+        }else{
+        for(int i=0; i<numeroCartas ; i++){
+            Scanner sc1 = new Scanner(System.in);
+            int[] conjuntoCartas = {sc1.nextInt()} ;
+        }
+        }
         return null;
     }
 
